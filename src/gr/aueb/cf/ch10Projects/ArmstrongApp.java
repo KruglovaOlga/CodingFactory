450023c233b6c0b384f2270b253c495b33c850a1
KruglovaOlga <std094996@ac.eap.gr>
2022-12-24 09:24:37 +0200

    mini Projects

--- src/gr/aueb/cf/ch10Projects/ArmstrongApp.java
+++ src/gr/aueb/cf/ch10Projects/ArmstrongApp.java
@@ -1,5 +1,11 @@
 package gr.aueb.cf.ch10Projects;
+/*
+Βρίσκει αν το άθροισμα των δυνάμεων κάθε ψηφίου εις τη (πλήθος ψηφίων)
+είναι ίσο με τον ίδιο τον αριθμό. Π.Χ. το 153 = 1^3 + 5^3 + 3^3. Επομένως
+το 153 είναι Armstrong number.
+Επίσης, τα 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634, 8208, 9474, 54748, 92727, 93084
 
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -32,3 +38,40 @@ public class ArmstrongApp {
         System.out.printf("%d is Armsrong: %s", inputNumber, (isArmstrong) ? "YES" : "FALSE");
     }
 }
+
+/************************************************************************
+ * import java.lang.Math;
+ * public class myClass
+ * {
+ *  public static void main(String[] args)
+ *  {
+ *   int num = 19;
+ *   int copyNum = num;
+ *   int digits = 0;
+ *   int remainder = 0;
+ *   int total = 0;
+ *
+ *   // find number of digits in num variable
+ *   while(copyNum != 0)
+ *   {
+ *    digits++;
+ *    copyNum = copyNum / 10;
+ *   }
+ *   copyNum = num;
+ *
+ *   // slice the numbers from last digits
+ *   while(copyNum != 0)
+ *   {
+ *    remainder = copyNum % 10;
+ *    total += (int) Math.pow(remainder, digits);
+ *    copyNum = copyNum / 10;
+ *   }
+ *
+ *   // result
+ *   if(num == total)
+ *    System.out.format("%d is an armstrong number", num);
+ *   else
+ *    System.out.format("%d is not an armstrong number", num);
+ *  }
+ * }
+ */
